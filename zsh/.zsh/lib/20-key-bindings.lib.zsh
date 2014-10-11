a7fdbb7d10d840bc39e5006d1eadbdf2d4b14392
Ádám Barancsuk <brncsk@users.noreply.github.com>
2014-10-11 16:41:49 +0200

    Zsh: sanitize and comment keybindings.

--- zsh/.zsh/lib/20-key-bindings.lib.zsh
+++ zsh/.zsh/lib/20-key-bindings.lib.zsh
@@ -1,30 +1,17 @@
-bindkey -e
-bindkey '\ew' kill-region
+# M-l for ls
 bindkey -s '\el' "ls\n"
+
+# M-. for cd ..
 bindkey -s '\e.' "..\n"
+
+# C-r for searching history
 bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
 
-# make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+# Perform history expansion on space
+bindkey ' ' magic-space
 
-bindkey "^[[H" beginning-of-line
-bindkey "^[[1~" beginning-of-line
-bindkey "^[OH" beginning-of-line
-bindkey "^[[F"  end-of-line
-bindkey "^[[4~" end-of-line
-bindkey "^[OF" end-of-line
-bindkey ' ' magic-space    # also do history expansion on space
-
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-
+# S-Tab for moving backwards in the completion menu
 bindkey '^[[Z' reverse-menu-complete
 
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char
