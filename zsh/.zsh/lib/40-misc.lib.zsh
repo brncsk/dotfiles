7f22273f5713039419b2cd96bfc5ee2146a7bb7c
Ádám Barancsuk <brncsk@users.noreply.github.com>
2016-09-30 07:19:08 +0200

    zsh: misc: vf() for opening matching files in vim automatically.

--- zsh/.zsh/lib/40-misc.lib.zsh
+++ zsh/.zsh/lib/40-misc.lib.zsh
@@ -87,6 +87,11 @@
 		(find ${2:=.} -print0 | xargs -0 grep -i $1 ) 2>/dev/null
 	}
 
+# Find in files then open them in vim
+  vf () {
+    vim -p $(ff $1 | cut -d':' -f1 | uniq)
+  }
+
 # Mass renaming with zmv
 
 	autoload -U zmv
